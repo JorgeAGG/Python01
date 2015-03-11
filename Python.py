545daf50f7adea97e0d6386fefc6f70916f592c3
JorgeAGG <jorge_94_aje@hotmail.com>
2015-03-11 11:54:14 +0100

    Python01
    Jorge A. García González

---
+++ Python.py
@@ -0,0 +1,16 @@
+#Jorge A. García González
+#Python01 - 99 bottle song.
+
+
+
+for i in reversed(range(1, 99)):
+    print i, "bottles of beer on the wall,", i, "bottles of beer"
+    print"Take one down, pass it around,", i-1, "bottles of beer on the wall.\n"
+
+
+
+
+
+
+
+
