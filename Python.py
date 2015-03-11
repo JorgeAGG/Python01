1b8c600a39f6fe27e1a96a5e9b23f56ca0c693d7
JorgeAGG <jorge_94_aje@hotmail.com>
2015-03-11 11:57:57 +0100

    Update Python.py

--- Python.py
+++ Python.py
@@ -1,7 +1,7 @@
 #Jorge A. García González
 #Python01 - 99 bottle song.
 
-for b in reversed(range(1, 99)):
+for b in reversed(range(1, 100)):
     print b, "bottles of beer on the wall,", b, "bottles of beer"
     print"Take one down, pass it around,", b-1, "bottles of beer on the wall.\n"
 
